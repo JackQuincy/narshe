15e2be9e03f6752440cb5b2e8c98f9dd532b08b0
asilverthorn <96998881+asilverthorn@users.noreply.github.com>
2023-08-16 20:10:19 -0600

    Renaming from app to application for Elastic Beanstalk support

---
+++ application.py
@@ -0,0 +1,40 @@
+from flask import Flask, make_response
+from dotenv import load_dotenv
+import api.metadata
+import api.generate
+import api.log
+import api.portrait
+import api.seed
+import api.sprite
+import api.sprites
+import api.wc
+
+# read in the env variables
+load_dotenv('.env')
+application = Flask(__name__)
+
+@application.route("/", methods=["GET"])
+def hello_world():
+    return "<p>Hello!</p>"
+
+#register the endpoints
+application.register_blueprint(api.generate.bp)
+
+application.register_blueprint(api.log.bp)
+
+application.register_blueprint(api.metadata.bp)
+
+@application.route("/api/music/generate", methods=["POST"])
+def post_generate_music():
+    return make_response("Unsupported", 500)
+
+application.register_blueprint(api.portrait.bp)
+
+application.register_blueprint(api.seed.bp)
+
+application.register_blueprint(api.sprite.bp)
+
+application.register_blueprint(api.sprites.bp)
+
+application.register_blueprint(api.wc.bp)
+
